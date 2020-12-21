94f81f3668e26a3da2265f6c5a406614bf912e3c
hypercyte <hypercyte@gmail.com>
2020-12-21 23:26:07 +0000

    Move input processing to separate files

---
+++ coursework/RobotWar/RobotWar/input_processing.h
@@ -0,0 +1,20 @@
+//
+//	input_processing.h
+//	Coursework: Robot War
+//
+//	Created by Mujahid Ahmed, 2020
+//
+
+#ifndef INPUT_PROCESSING_H
+#define INPUT_PROCESSING_H
+
+#include <vector>
+#include <string>
+
+void input_data(std::vector<std::string>& v, const std::string& dir);
+
+std::vector<std::string> seperate(const std::string& s);
+
+#endif // !INPUT_PROCESSING_H
+
+
--- coursework/RobotWar/RobotWar/main.cpp
+++ coursework/RobotWar/RobotWar/main.cpp
@@ -6,6 +6,7 @@
 //
 
 #include "robot.h"
+#include "input_processing.h"
 
 #include <iostream>
 #include <fstream>
@@ -15,36 +16,9 @@
 
 using namespace std;
 
-/*=====================================
-read input files and separate the lines
-=======================================*/
-void input_data(vector<string>& v, const string& dir) {
-	ifstream file(dir); // load file
-	string s; // init string
-	while (getline(file, s)) {
-		v.push_back(s); // push each line into vector
-	}
-}
-
-/*=====================================
-separate words at every space character
-=======================================*/
-vector<string> seperate(const string& s) {
-	vector<string> v; // empty vector of type int
-	auto space = find(s.cbegin(), s.cend(), ' '); // find the first instance of a space character
-	v.push_back(string(s.cbegin(), space)); // convert first string to int and push it into vector v
-	// continue till the end
-	while (space != s.cend()) { 
-		auto start = ++space;
-		space = find(start, s.cend(), ' ');
-		v.push_back(string(start, space));
-	}
-	return v; // return the vector with separated ints
-}
-
-/*=========
-main
-==========*/
+/*===========
+main program
+=============*/
 int main() {
 	vector<string> start; // empty vector of strings
 	vector<string> commands;
